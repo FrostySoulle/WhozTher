ef0c77ef4e1a1ba2c281aea12ad3250eb1d75d98
FrostySoulle <68221092+FrostySoulle@users.noreply.github.com>
2026-01-29 15:22:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', msg => {
+  if (msg.content === '!ping') msg.reply('pong');
+});
+
+client.login(process.env.TOKEN);
